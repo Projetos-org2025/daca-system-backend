368b29c717df431db51a846530630edcb3766ccc
Diego Araujo <sabonightscortes@gmail.com>
2025-06-12 11:32:38 -0300

    Corrige erro de indentação em CoordenadaCache

--- models.py
+++ models.py
@@ -2,38 +2,33 @@ from sqlmodel import SQLModel, Field
 from typing import Optional, Annotated
 
 class Cliente(SQLModel, table=True):
-id: Optional\[int] = Field(default=None, primary\_key=True)
-login: Annotated\[str, Field(min\_length=3, max\_length=50)]
-senha: Annotated\[str, Field(min\_length=3)]
+    id: Optional[int] = Field(default=None, primary_key=True)
+    login: Annotated[str, Field(min_length=3, max_length=50)]
+    senha: Annotated[str, Field(min_length=3)]
 
 class Veiculo(SQLModel, table=True):
-id: Optional\[int] = Field(default=None, primary\_key=True)
-placa: Annotated\[str, Field(min\_length=3)]
-marca: str
-modelo: str
-ano: Annotated\[int, Field(ge=1900, le=2100)]
-login\_cliente: Annotated\[str, Field(min\_length=3, max\_length=50)]  # Dono do veículo
-imei: Annotated\[str, Field(min\_length=10, max\_length=30)]          # IMEI do rastreador
+    id: Optional[int] = Field(default=None, primary_key=True)
+    placa: Annotated[str, Field(min_length=3)]
+    marca: str
+    modelo: str
+    ano: Annotated[int, Field(ge=1900, le=2100)]
+    login_cliente: Annotated[str, Field(min_length=3, max_length=50)]  # Dono do veículo
+    imei: Annotated[str, Field(min_length=10, max_length=30)]          # IMEI do rastreador
 
 class Localizacao(SQLModel, table=True):
-id: Optional\[int] = Field(default=None, primary\_key=True)
-latitude: float
-longitude: float
-velocidade: float
-data\_hora: str
-ignicao: str
-operadora: str
-endereco: Optional\[str] = None
-login: Optional\[str] = None
-placa: Optional\[str] = None
+    id: Optional[int] = Field(default=None, primary_key=True)
+    latitude: float
+    longitude: float
+    velocidade: float
+    data_hora: str
+    ignicao: str
+    operadora: str
+    endereco: Optional[str] = None
+    login: Optional[str] = None
+    placa: Optional[str] = None
 
 class CoordenadaCache(SQLModel, table=True):
-
     id: Optional[int] = Field(default=None, primary_key=True)
-
     lat: str
-
     lon: str
-
     endereco: str
-
